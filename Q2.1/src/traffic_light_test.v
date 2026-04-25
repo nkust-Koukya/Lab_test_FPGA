9da1157fa15340a1ac2dca8473dbc2ffd52a5ddb
nkust-Koukya <c110112158@nkust.edu.tw>
2026-04-26 02:40:14 +0800

    Add files via upload

---
+++ Q2.1/src/traffic_light_test.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module trafficLight_testbench( );
+	reg  clk, reset ; 
+	wire [1:0] light;
+	
+	trafficlight  uut (clk, reset , light);
+	
+	initial begin
+		clk = 0;
+		reset = 0;
+	end
+	
+	initial begin
+		#10 reset = 1;
+		#10 reset = 0;
+		#1200 reset = 1;
+		#10 reset = 0; 
+	end
+	
+	initial forever #10 clk = ~clk;
+	initial $monitor($time, " light = %b", light);
+	initial #1500 $finish;
+	
+		
+endmodule
